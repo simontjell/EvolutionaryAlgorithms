9b6742faa54af398416ba331ee3ad56a3054f9a1
Simon Tjell <simon@simplesystemer.dk>
2018-02-21 11:08:44 +0100

    Basic flow now works - with a naive example

--- src/CSharpDE/UnitTest1.cs
+++ src/CSharpDE/UnitTest1.cs
@@ -12,11 +12,35 @@ namespace CSharpDE
         [Fact]
         public void Test1()
         {
-            var de = new DifferentialEvolution(
-                new MyOptimizationProblem(), 
+            //var de = new DifferentialEvolution(
+            //    new MyOptimizationProblem(), 
+            //    new OptimizationParameters { PopulationSize = 100 }
+            //);
+
+            var optimizationAlgorithm = new NaiveOptimizationAlgorithm(
+                new MyOptimizationProblem(),
                 new OptimizationParameters { PopulationSize = 100 }
             );
+
+            optimizationAlgorithm.Optimize();
+
         }
+    }
+
+    public class NaiveOptimizationAlgorithm : EvolutionaryAlgorithm
+    {
+        public NaiveOptimizationAlgorithm(OptimizationProblem optimizationProblem, OptimizationParameters optimizationParameters) : base(optimizationProblem, optimizationParameters){ }
+
+        protected override ImmutableList<Offspring> CreateOffspring(ImmutableList<Individual> parents)
+            => parents
+               .Select(p => new Offspring(p, _optimizationProblem.CreateRandomIndividual()))
+               .ToImmutableList();
+
+        protected override bool ReplaceParents(Offspring offsprintIndividual, ImmutableDictionary<Individual, double> offspringFitnessValues)
+            => offspringFitnessValues[offsprintIndividual] < offsprintIndividual.Parents.Select(p => FitnessValues[p]).Min();
+
+        protected override ImmutableList<Individual> SelectParents()
+            => Generations.Last().Population;
     }
 
     public abstract class OptimizationProblem
@@ -28,12 +52,12 @@ namespace CSharpDE
 
     public abstract class EvolutionaryAlgorithm
     {
-        private readonly OptimizationProblem _optimizationProblem;
-        private readonly OptimizationParameters _optimizationParameters;
+        protected readonly OptimizationProblem _optimizationProblem;
+        protected readonly OptimizationParameters _optimizationParameters;
 
         public ImmutableList<Generation> Generations { get; protected set; }
 
-        public ImmutableDictionary<Individual, double> FitnessValues { get; protected set; };
+        public ImmutableDictionary<Individual, double> FitnessValues { get; protected set; }
 
         protected EvolutionaryAlgorithm(OptimizationProblem optimizationProblem, OptimizationParameters optimizationParameters)
         {
@@ -51,29 +75,54 @@ namespace CSharpDE
             FitnessValues = Generations.Single().Population.ToImmutableDictionary(individual => individual, individual => _optimizationProblem.CalculateFitnessValue(individual));
 
             // Step Three: Repeat the following regenerational steps until termination:
-            while(_optimizationParameters.TerminationCriteria.All(criterion => criterion.ShouldTerminate(this) == false))
+            while (_optimizationParameters.TerminationCriteria?.All(criterion => criterion.ShouldTerminate(this)) ?? false == false)
             {
+                var bestFitness = FitnessValues.Values.Min();
+
                 // Select the best - fit individuals for reproduction. (Parents)
                 var parents = SelectParents();
+
                 // Breed new individuals through crossover and mutation operations to give birth to offspring.
+                var offspring = CreateOffspring(parents);
+
                 // Evaluate the individual fitness of new individuals.
-                // Replace least - fit population with new individuals.
+                var offspringFitnessValues = offspring.Cast<Individual>().ToImmutableDictionary(individual => individual, individual => _optimizationProblem.CalculateFitnessValue(individual));
+
+                // Replace least-fit population with new individuals.
+                var newPopulation = new List<Individual>();
+                var newFitnessValues = new List<KeyValuePair<Individual, double>>();
+
+                foreach (var offsprintIndividual in offspring)
+                {
+                    if (ReplaceParents(offsprintIndividual, offspringFitnessValues))
+                    {
+                        newPopulation.Add(offsprintIndividual);
+                        newFitnessValues.Add(new KeyValuePair<Individual, double>(offsprintIndividual, offspringFitnessValues[offsprintIndividual]));
+                    }
+                    else
+                    {
+                        newPopulation.AddRange(offsprintIndividual.Parents);
+                    }
+                }
+
+                FitnessValues = FitnessValues.AddRange(newFitnessValues);
+                Generations = Generations.Add(new Generation(newPopulation.ToImmutableList()));
             }
 
             throw new NotImplementedException();
         }
 
+        protected abstract bool ReplaceParents(Offspring offsprintIndividual, ImmutableDictionary<Individual, double> offspringFitnessValues);
+        protected abstract ImmutableList<Offspring> CreateOffspring(ImmutableList<Individual> parents);
         protected abstract ImmutableList<Individual> SelectParents();
 
         protected virtual Generation InitializeFirstGeneration()
         {
-            return new Generation
-            {
-                Population =
-                    Enumerable.Range(0, _optimizationParameters.PopulationSize)
-                    .Select(i => _optimizationProblem.CreateRandomIndividual())
-                    .ToList()
-            };
+            return new Generation(
+                Enumerable.Range(0, _optimizationParameters.PopulationSize)
+                .Select(i => _optimizationProblem.CreateRandomIndividual())
+                .ToImmutableList()
+            );
         }
     }
 
@@ -87,18 +136,19 @@ namespace CSharpDE
 
     public abstract class TerminationCriterion
     {
-                public abstract bool ShouldTerminate(EvolutionaryAlgorithm optimizationAlgorithm);
+        public abstract bool ShouldTerminate(EvolutionaryAlgorithm optimizationAlgorithm);
     }
 
-    public class DifferentialEvolution : EvolutionaryAlgorithm
-    {
-        public DifferentialEvolution(OptimizationProblem optimizationProblem, OptimizationParameters optimizationParameters) : base(optimizationProblem, optimizationParameters)
-        {
-        }
+    //public class DifferentialEvolution : EvolutionaryAlgorithm
+    //{
+    //    public DifferentialEvolution(OptimizationProblem optimizationProblem, OptimizationParameters optimizationParameters) : base(optimizationProblem, optimizationParameters){ }
 
-        protected override ImmutableList<Individual> SelectParents()
-            => Generations.Last().Population;   // Take all...S
-    }
+    //    protected override ImmutableList<Individual> SelectParents()
+    //        => Generations.Last().Population;   // Take all...
+
+    //    protected override ImmutableList<Offspring> CreateOffspring(ImmutableList<Individual> parents)
+    //        => throw new NotImplementedException();
+    //}
     
     
     public class MyOptimizationProblem : OptimizationProblem
@@ -117,17 +167,37 @@ namespace CSharpDE
 
     public class Individual
     {
-        private readonly List<double> _genes;
-        public double this[int index] => _genes[index];
+        public readonly ImmutableList<double> Genes;
+        public double this[int index] => Genes[index];
 
         public Individual(params double[] genes)
         {
-            _genes = genes.ToList();
+            Genes = genes.ToImmutableList();
         }
+    }
+
+    public class Offspring : Individual
+    {
+        public ImmutableList<Individual> Parents { get; private set; }
+
+        public Offspring(Individual parent, params double[] genes) : this(new List<Individual> { parent }.ToImmutableList(), genes) { }
+
+        public Offspring(Individual parent, Individual individual) : this(parent, individual.Genes.ToArray()) { }
+
+        public Offspring(ImmutableList<Individual> parents, params double[] genes) : base(genes)
+        {
+            Parents = parents;
+        }
+
     }
 
     public class Generation
     {
-        public ImmutableList<Individual> Population { get; set; }
+        public Generation(ImmutableList<Individual> population)
+        {
+            Population = population;
+        }
+
+        public ImmutableList<Individual> Population { get; private set; }
     }
 }
