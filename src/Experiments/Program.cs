4c8384c24337924995a1f832108c1851f824c6e4
Simon Tjell <simon@simplesystemer.dk>
2018-02-23 22:22:18 +0100

    Added a simple multi-objective benchmark problem - some details are still missing in the handling of such problems

--- src/Experiments/Program.cs
+++ src/Experiments/Program.cs
@@ -11,12 +11,12 @@ namespace Experiments
         static void Main(string[] args)
         {
             var optimizationAlgorithm = new DifferentialEvolution(
-                            new MyOptimizationProblem(),
-                            new DifferentialEvolutionOptimizationParameters(
-                                100,
-                                new LambdaTerminationCriterion(algorithm => algorithm.GetBestIndividuals(algorithm.Generations.Last()).Single().FitnessValues.Single() == 0.0)  // TODO: Rethink the interface for getting best fit individual(s)
-                            )
-                        );
+                new SchafferFunctionOptimizationProblem(),
+                    new DifferentialEvolutionOptimizationParameters(
+                        100,
+                        new LambdaTerminationCriterion(algorithm => algorithm.GetBestIndividuals(algorithm.Generations.Last()).Single().FitnessValues.Single() == 0.0)  // TODO: Rethink the interface for getting best fit individual(s)
+                    )
+                );
 
             optimizationAlgorithm.Optimize();
         }
@@ -33,6 +33,30 @@ namespace Experiments
             public override bool IsFeasible(Individual individual) => true;
             public override ImmutableList<double> CalculateFitnessValue(Individual individual) => new List<double> { Math.Pow(individual[0] * individual[1], 2.0) }.ToImmutableList();
             public override Individual CreateRandomIndividual() => new Individual(_rnd.NextDouble(), _rnd.NextDouble());
+        }
+
+        // https://en.wikipedia.org/wiki/Test_functions_for_optimization#Test_functions_for_multi-objective_optimization
+        public class SchafferFunctionOptimizationProblem : OptimizationProblem
+        {
+            private readonly Random _rnd;
+
+            public SchafferFunctionOptimizationProblem()
+            {
+                _rnd = new Random((int)DateTime.Now.Ticks);
+            }
+
+            public override ImmutableList<double> CalculateFitnessValue(Individual individual)
+            {
+                return new List<double> { 
+                    Math.Pow(individual[0], 2.0),
+                    Math.Pow(individual[0] - 2, 2.0)
+                }.ToImmutableList();
+            }
+
+            public override Individual CreateRandomIndividual()
+            {
+                return new Individual(_rnd.NextDouble() * 10.0, _rnd.NextDouble() * 10.0);
+            }
         }
 
     }
