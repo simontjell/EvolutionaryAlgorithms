4c4054fb31919594c0f731c517e4cc12137bbfa0
Simon Tjell <simon@simplesystemer.dk>
2021-10-14 20:23:08 +0200

    A rather naïve test of pareto covergence using a simple multi-objective test problem

--- tests/DifferentialEvolution.Tests/OptimizationProblemTests.cs
+++ tests/DifferentialEvolution.Tests/OptimizationProblemTests.cs
@@ -4,6 +4,7 @@ using Xunit;
 using EvolutionaryAlgorithm;
 using EvolutionaryAlgorithm.TerminationCriteria;
 using System.Linq;
+using System.Collections.Immutable;
 
 namespace DifferentialEvolution.Tests
 {
@@ -72,5 +73,43 @@ namespace DifferentialEvolution.Tests
             Assert.Equal(0.0, finalBestIndividual.Genes[1], 2);
             Assert.Equal(0.0, finalBestIndividual.FitnessValues.Single(), 2);
         }
+
+        [Fact]
+        public void When_optimization_problem_is_Schaffer_DE_converges_correctly()
+        {
+            // Arrange
+            var rnd = new Random(0);
+            var populationSize = 100;
+            var sut = new DifferentialEvolution(
+                new SchafferFunctionOptimizationProblem(rnd),
+                new DifferentialEvolutionOptimizationParameters(
+                    populationSize,
+                    new GenerationCountTerminationCriterion(100)
+                ),
+                rnd
+            );
+
+            // Act
+            IImmutableList<ParetoEvaluatedIndividual> GetBestIndividuals()
+                => sut.GetBestIndividuals(sut.Generations.Last());
+
+            sut.OnGenerationFinished += (s, e) => {
+                var bestIndividuals = GetBestIndividuals();
+                var paretoFronts = sut.Generations.Last().Population.GroupBy(p => p.ParetoRank).Select(g => new { ParetoRank = g.Key, NumberOfIndividuals = g.Count() } ).OrderBy(p => p.ParetoRank).ToList();
+                Console.WriteLine($"{nameof(SchafferFunctionOptimizationProblem)} {sut.Generations.Count}: {string.Join(", ", paretoFronts.Select(p => $"{p.ParetoRank}: {p.NumberOfIndividuals}"))}");
+            };
+
+            sut.Optimize();
+            System.IO.File.WriteAllLines("schaffer_front_0.csv", finalBestIndividuals.Select(p => $"{p.FitnessValues[0]},{p.FitnessValues[1]}"));
+
+            // Assert
+            var finalBestIndividuals = GetBestIndividuals();
+            Assert.Equal(0, finalBestIndividuals.Select(i => i.ParetoRank).Distinct().Single());
+            Assert.True(finalBestIndividuals.Count >= populationSize / 2);
+            Assert.Equal(populationSize, sut.Generations.Last().Population.Count);
+            Assert.True(finalBestIndividuals.Where(i => i.FitnessValues[0] > 1.0).All(i => i.FitnessValues[1] < 1.0));
+            Assert.True(finalBestIndividuals.Where(i => i.FitnessValues[1] > 1.0).All(i => i.FitnessValues[0] < 1.0));
+        }
+
     }
 }
--- tests/DifferentialEvolution.Tests/OptimizationProblems/SchafferFunctionOptimizationProblem.cs
+++ tests/DifferentialEvolution.Tests/OptimizationProblems/SchafferFunctionOptimizationProblem.cs
@@ -8,7 +8,17 @@ namespace DifferentialEvolution.Tests.OptimizationProblems
     // https://en.wikipedia.org/wiki/Test_functions_for_optimization#Test_functions_for_multi-objective_optimization
     public class SchafferFunctionOptimizationProblem : OptimizationProblem
     {
-        public SchafferFunctionOptimizationProblem(Random rnd) : base(2, rnd){}
+        private const double MinimumA = 10.0;
+        private double _a;
+
+        public SchafferFunctionOptimizationProblem(Random rnd, double a = MinimumA) : base(2, rnd)
+        {
+            if(a < MinimumA)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(a)} should be at least {MinimumA}");
+            }
+            _a = a;
+        }
 
         public override ImmutableList<double> CalculateFitnessValues(Individual individual)
             => new List<double> {
@@ -17,7 +27,10 @@ namespace DifferentialEvolution.Tests.OptimizationProblems
             }.ToImmutableList();
 
         public override Individual CreateRandomIndividual()
-            => new Individual(_rnd.NextDouble() * 10.0, _rnd.NextDouble() * 10.0);
+            => new Individual(-_a + (_rnd.NextDouble() * 2.0 * _a), -_a + (_rnd.NextDouble() * 2.0 * _a));
+
+        public override bool IsFeasible(Individual individual)
+            => individual.Genes[0] >= -_a && individual.Genes[0] <= _a;
     }
 
 }
